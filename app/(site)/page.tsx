608e8f3982dd9c362aab5d89cd8933558bccd9fc
urhitech <admin@urhitech.com>
2023-05-30 16:28:42 +0000

    Header created

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,9 @@
+import { Header } from "@/components";
 
 export default function Home() {
   return (
-    <div className="text-green-500">Main Contents</div>
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>Header</Header>
+    </div>
   )
 }
--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Sidebar } from './Sidebar';
 export { default as Box } from './Box';
 export { default as SidebarItem } from './SidebarItem';
 export { default as Library } from './Library';
+export { default as Header } from './Header';
